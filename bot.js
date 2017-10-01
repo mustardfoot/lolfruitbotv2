dfaaeca37e0d94911ee6bedf569d38ef2bd53416
mustardfoot <mustardfoot@gmail.com>
2017-10-01 14:31:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('ready', () => {
-	console.log("ok letsa go");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-bot.on('message', message => {
-	if(message.author === bot.user) return;
-  if(message.guild && message.guild.id === '355836687777267712' && message.guild.available === true) {
-    if (message.content === 'knife me daddy') {
-      message.channel.send('*knifes*');
-    }
-  }
- });
-
-client.login(process.env.BOT_TOKEN)
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
