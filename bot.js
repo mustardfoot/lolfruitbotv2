b0d12bd3f8df16cf051f40584f0c6bdb3d468c92
mustardfoot <mustardfoot@gmail.com>
2017-10-05 19:52:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,6 +109,7 @@ client.on('message', function(message) {
         var userlist = message.mentions.members; // Saving userlist to a variable
         userlist.forEach(function(user){
           user.addRole(message.member.guild.roles.find("name","buyers"));
+          cmdoutput('Whitelist',"<@"+user.id+"> has been whitelisted for Grab Knife V4, please DM "+message.author.name+" with your desired username and password.",message.channel)
         })
       }
       break;
