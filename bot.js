280640a82efa91692a58494d054ff51bdcb38e7e
mustardfoot <mustardfoot@gmail.com>
2017-10-05 20:18:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,9 +110,9 @@ client.on('message', function(message) {
         userlist.forEach(function(user){
           user.addRole(message.member.guild.roles.find("name","buyers"));
           cmdoutput('Whitelist',"<@"+user.id+"> has been whitelisted for Grab Knife V4.",message.channel);
-          user.user.createDM(boi){
-           cmdoutput("You have been whitelisted for Grab Knife V4, please DM <@"+message.author.id+"> with your desired username and password.",boi); 
-          }
+          user.user.createDM().then((boi) => {
+          cmdoutput('Whitelist',"You have been whitelisted for Grab Knife V4, please DM <@"+message.author.id+"> with your desired username and password to get started.",boi);
+          });
         })
       }
       break;
