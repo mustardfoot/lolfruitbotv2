79eb0c7058af744eab3470036705cef6b86e80c1
mustardfoot <mustardfoot@gmail.com>
2017-10-08 12:46:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -278,8 +278,9 @@ client.on('message', function(message) {
       break;
     case "mute" :
       console.log(args[1].substring(2,args[1].length-1))
-      var muser = client.fetchUser(args[1].substring(2,args[1].length-1));
+      var muser = client.fetchUser(args[1].substring(2,args[1].length-1)).then((boi) => {
       muser = message.guild.fetchMember(muser);
+      });
       var reason = "No Reason Provided";
       var curnum = 1;
       args.forEach(function(role){
