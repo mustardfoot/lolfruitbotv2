73e27b6427ea7a9f409829d2c737ef72ac21c9f7
mustardfoot <mustardfoot@gmail.com>
2017-10-16 18:36:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,9 @@ function updatejoker(){
        title: "Joker Battle!",
        description: "Spam :punch: in this channel to stop him!!",
        timestamp: new Date(),
-       icon: "https://static.comicvine.com/uploads/original/11125/111253442/5004137-joker.jpg",
+       thumbnail: {
+        url: "https://static.comicvine.com/uploads/original/11125/111253442/5004137-joker.jpg"
+       },
        fields: [{
         name: "Joker HP:",
         value: jokerhp+"/"+jokermax
@@ -213,7 +215,6 @@ client.on('message', function(message) {
   }
   if(message.channel === jokerchannel){
    if(jokering === true){
-    console.log(message.content);
    var count1 = occurrences(message.content,"👊");
    jokerhp = jokerhp - count1;
    }else{
@@ -467,7 +468,9 @@ var myInterval = setInterval(function() {
        color: 14680064,
        title: "Joker Battle!",
        description: "The joker has been defeated!",
-       icon: "https://i.pinimg.com/736x/86/22/ae/8622ae3e39fbb2b2ebf9afa6b12befa5--dc-comic-comic-art.jpg",
+       thumbnail: {
+        url: "https://i.pinimg.com/736x/86/22/ae/8622ae3e39fbb2b2ebf9afa6b12befa5--dc-comic-comic-art.jpg"
+       },
        timestamp: new Date()
       }});
      }else{
