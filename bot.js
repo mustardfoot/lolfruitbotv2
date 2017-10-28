cc582bf9acf7d33108f98d5ffd5edb5db1478f21
mustardfoot <mustardfoot@gmail.com>
2017-10-28 18:49:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,16 +287,17 @@ client.on('message', function(message) {
                   }
                 })
                 if(hwids){
-                  var cards = t.get("/1/lists/"+hwids+"/cards?fields=id,name,desc");
-                  var found = false;
-                  cards.forEach(function(card){
-                    if (card.desc === authid){
-                      message.channel.send("You're already whitelisted! Please run the command !removewhitelist if you want to change it.")
+                  t.get("/1/lists/"+hwids+"/cards?fields=id,name,desc",function(err,cards){
+                    var found = false;
+                    cards.forEach(function(card){
+                      if (card.desc === authid){
+                        message.channel.send("You're already whitelisted! Please run the command !removewhitelist if you want to change it.")
+                      }
+                    })
+                    if(found === false){
+                      t.post('/1/cards?name='+args[1]+'&desc='+authid+'&pos=top&idList='+hwids, success, error);
                     }
-                  })
-                  if(found === false){
-                    t.post('/1/cards?name='+args[1]+'&desc='+authid+'&pos=top&idList='+hwids, success, error);
-                  }
+                  });
                 }else{
                   message.channel.send("Something seems to be wrong with the HWID list! Please contact mustardfoot and tell him!")
                 }
