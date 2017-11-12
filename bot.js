a3bfdb0cac218254863148e0694f87e528b93e5d
mustardfoot <mustardfoot@gmail.com>
2017-11-12 11:02:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -826,7 +826,7 @@ var myInterval = setInterval(function() {
       t.get("/1/lists/"+hwids+"/cards?fields=id,name,desc",function(err,cards){
         cards.forEach(function(card){
           t.get('1/cards/'+card.id+'/dateLastActivity',function(err,date){
-            var goaltime = mew Date(date);
+            var goaltime = new Date(date);
             var todaymin = goaltime.getMinutes()+parseInt(card.desc);
             if(todaymin <= m){
               console.log(card.name);
