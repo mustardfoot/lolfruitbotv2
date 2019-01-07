a6015eb5f67ed7d0616086b003efe237b2f8508d
mustardfoot <mustardfoot@gmail.com>
2019-01-06 22:14:16 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1001,8 +1001,7 @@ Noblox.cookieLogin(process.env.ROBLOSECURITY)
               .then((results) => {
                 results.logs.forEach(function(result){
                   console.log('---')
-                  console.log(result.user.name);
-                  console.log(result.action);
+                  console.log(result.text)
                 })
                 //switch (){}
                 var firstcard = cards[0];
