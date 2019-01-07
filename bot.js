7fd1011dd66debb44884b287719f4ca1dc24ce61
mustardfoot <mustardfoot@gmail.com>
2019-01-06 22:46:45 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1003,6 +1003,7 @@ Noblox.cookieLogin(process.env.ROBLOSECURITY)
                   if(result.text.substring(0,85) === "mustardfoot spent 5 of group funds for: one-time payout of Robux from group funds to "){
                     var theirname = result.text.substring(85, result.text.length-result.text.split(" ")[2].length-4);
                     if(theirname === cards[0].desc.substring(6,theirname.length+6)){
+                      console.log('what the fUCK');
                       t.put('1/cards/'+cards[0].id+'/'+requests2,function(err,returns){});
                     }
                   }
